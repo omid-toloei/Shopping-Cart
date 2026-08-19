64acc627c4d4af98033bcecbf0addb5741a105eb
Omid-Toloei <omidtoloei.dev@gmail.com>
2026-08-19 21:40:49 +0330

    add: loader spinner

--- javascript/main.js
+++ javascript/main.js
@@ -6,11 +6,14 @@
 const showErrorSection = document.querySelector(".show-error-section");
 const showErrorMessage = document.querySelector("#showErrorMessage");
 
+// Loader spinner
+const loaderParent = document.querySelector("#loaderParent");
+
 /* =================================
   Global Variables
 ================================= */
 
-
+let isLoading = false;
 
 /* =================================
   Event Listeners
@@ -23,24 +26,28 @@ const showErrorMessage = document.querySelector("#showErrorMessage");
 ================================= */
 
 async function request(url, options) {
+  if (isLoading) return;
+  isLoading = true;
+  showLoading(isLoading);
+
   try {
     let response = await fetch(url, options);
 
-    if(!response.ok) {
+    if (!response.ok) {
       throw response.status;
     }
 
     let dataJson = await response.json();
     return await dataJson;
-    
-  } catch(error) {
-    if(error instanceof TypeError) {
+  } catch (error) {
+    if (error instanceof TypeError) {
       throw "NETWORK_ERROR";
     } else {
       throw error;
     }
   } finally {
-
+    isLoading = false;
+    showLoading(isLoading);
   }
 }
 
@@ -51,7 +58,7 @@ async function request(url, options) {
 function showError(errorType) {
   let errorMessage = "";
 
-  switch(errorType) {
+  switch (errorType) {
     case "NETWORK_ERROR":
       errorMessage = "Please check your network connection";
       break;
@@ -70,4 +77,12 @@ function showError(errorType) {
 
   showErrorSection.style.display = "flex";
   showErrorMessage.textContent = errorMessage;
+}
+
+function showLoading(status) {
+  if (status) {
+    loaderParent.style.display = "flex";
+  } else {
+    loaderParent.style.display = "none";
+  }
 }
